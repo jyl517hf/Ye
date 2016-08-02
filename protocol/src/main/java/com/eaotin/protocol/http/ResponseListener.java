a5e6a17711ad9f0c1382a31d9f15dad6ecb6e3fb
jyl517hf <jyl517hf@126.com>
2016-08-02 15:00:43 +0800

    first commit

---
+++ protocol/src/main/java/com/eaotin/protocol/http/ResponseListener.java
@@ -0,0 +1,12 @@
+package com.eaotin.protocol.http;
+
+/**
+ * XXX.java是趣猜App的任务逻辑类。
+ *
+ * @author 金玉龙
+ * @version 2.0 16/8/2
+ * @modify
+ */
+public interface ResponseListener {
+    void onResponse(String requestId);
+}
---
+++ protocol/src/main/java/com/eaotin/protocol/http/ResultCode.java
@@ -0,0 +1,20 @@
+package com.eaotin.protocol.http;
+
+/**
+ * XXX.java是趣猜App的任务逻辑类。
+ *
+ * @author 金玉龙
+ * @version 2.0 16/8/2
+ * @modify
+ */
+public class ResultCode {
+    private ResultCode(){
+
+    }
+
+    /**
+     * 由于异常产生的本地错误码
+     */
+    public static final int EXCEPTION_ERROR = 10000;
+
+}
